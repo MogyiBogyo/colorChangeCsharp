5029e4c43d3312e33988152032c417f10c7b3f3f
a2c57g <vecsei.agnes96@gmail.com>
2020-07-22 14:13:36 +0200

    color change in console

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace csharpgyak2
+{
+    class Program
+    {
+        static ConsoleColor color = ConsoleColor.Blue;
+        static int index = 0;
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.Title = "szín beállító";
+            Console.CursorVisible = false;
+            do
+            {
+                Kiir();
+            } while (true);
+
+        }
+        static void Kiir()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                if(i == index)
+                {
+                    Console.BackgroundColor = color;
+                    Console.WriteLine(((ConsoleColor)i).ToString().PadLeft(15));
+                    Console.ResetColor();
+                }
+                else Console.WriteLine(((ConsoleColor)i).ToString().PadLeft(15));
+            }
+            Iranyitas();
+            Console.Clear();
+        }
+        static void Iranyitas()
+        {
+            ConsoleKey ck = Console.ReadKey().Key;
+            switch (ck)
+            {
+                case ConsoleKey.UpArrow:
+                    if (index-- == -1) index = 15;
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (index++ == 16) index = 0;
+                    break;
+                case ConsoleKey.Enter:
+                    color = (ConsoleColor)index;
+                    break;
+            }
+        }
+    }
+}
